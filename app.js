4fd733c1586f3795f6b2776c5ee1d62302c0c6c9
harshneu404 <agrawal.harsh@northeastern.edu>
2024-02-07 16:12:59 -0500

    update routes and error handling

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require ("express");
 const bodyParser = require("body-parser");
-
+const {connectDb} = require('./src/model/index');
 require("dotenv").config();
 const routes = require('./src/routes/index');
 const app = express();
+
+
 
 app.use(bodyParser.json());
 app.use((err,req,res,next)=>{
@@ -20,7 +22,10 @@ app.use((err,req,res,next)=>{
 routes(app);
 app.get("/",(req,res)=>{
     res.json({message:"Welcome to Network structures And Cloud Computing"});
-})
+});
+app.all('*', function(req, res){
+    res.status(404).send();
+  });
 const PORT = process.env.PORT||3000;
 app.listen(PORT,()=>{
     console.log(`Server is listening on port ${PORT}`);
