7ccffe352ad785adfbf664c1cecc591aa2db6610
harshneu404 <agrawal.harsh@northeastern.edu>
2024-02-15 13:26:25 -0500

    add settimeout

--- test/user.test.js
+++ test/user.test.js
@@ -26,6 +26,8 @@ describe("test_user_apis" , ()=>{
 
     beforeAll(async()=>{
         db.sequelize.sync({force:false});
+        async () => {await new Promise((resolve) => setTimeout(resolve, 4000));}
+
    });
     it("should create user in the db, and return proper values", async()=>{
     
