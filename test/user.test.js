e40ed08841478f43fb9f72b1a2ec950913cb1cdf
harshneu404 <agrawal.harsh@northeastern.edu>
2024-02-15 13:21:14 -0500

    update test file

--- test/user.test.js
+++ test/user.test.js
@@ -1,20 +1,21 @@
 const superTest = require('supertest');
 const {server,app} = require('../app');
-// const {db,connectDb} = require('../src/model');
+const {db,connectDb} = require('../src/model');
 const { faker } = require('@faker-js/faker');
 require('dotenv').config();
 const Sequelize = require("sequelize");
 
 
-const db = process.env.DB || 'demoDb'
-const dbUser = process.env.USER_NAME || "harsh"
-const  dbPassword = process.env.PASSWORD || "harsh" 
-const port = process.env.port || 3306
+// const db = process.env.DB || 'demoDb'
+// const dbUser = process.env.USER_NAME || "harsh"
+// const  dbPassword = process.env.PASSWORD || "harsh" 
+// const port = process.env.port || 3000
+// const host = process.env.HOST
 
-const sequelize =  new Sequelize(db,dbUser,dbPassword,{
-    dialect :'mysql',
-    logging: false
-})
+// const sequelize =  new Sequelize(db,dbUser,dbPassword,{
+//     dialect :'mysql',
+//     host:host
+// })
 const user = {
     firstName :faker.person.firstName(),
     lastName : faker.person.lastName(),
@@ -23,14 +24,6 @@ const user = {
 }
 describe("test_user_apis" , ()=>{
 
-    beforeAll(async()=>{
-        await sequelize.sync({force:false});
-
-        async () => {await new Promise((resolve) => setTimeout(resolve, 4000));}
-
-      
-    });
-   
     it("should create user in the db, and return proper values", async()=>{
     
          const user1 = await superTest(app)
@@ -55,7 +48,7 @@ describe("test_user_apis" , ()=>{
 describe("test_update_apis" , ()=>{
 
     beforeAll(async()=>{
-         sequelize.sync({force:false});
+         db.sequelize.sync({force:false});
     });
     afterAll(async() => {
        
@@ -63,7 +56,7 @@ describe("test_update_apis" , ()=>{
     })
     it("should update user in the db, and return proper values", async()=>{
         const firstName = "xyz";
-         const user1 = await superTest(app)
+          await superTest(app)
         .put('/v1/user/self')
         .set("Authorization", "basic " +  Buffer(`${user.emailId}:${user.password}`).toString("base64"))
         .send({firstName})
