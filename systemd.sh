146eb2248b02a62c704de2af987796e1a3b55089
harshneu404 <agrawal.harsh@northeastern.edu>
2024-03-01 15:26:44 -0500

    configure files

--- systemd.sh
+++ systemd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 sudo cp /opt/webapp/node_server.service /etc/systemd/system/node_server.service 
 sudo systemctl daemon-reload
-sudo systemctl enable node_server
+
+# sudo systemctl enable node_server
