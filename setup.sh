89229695cf26bb149bc11641c7ec33ba10475e50
harshneu404 <agrawal.harsh@northeastern.edu>
2024-03-18 21:59:16 -0400

    install ops agent

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 # echo "in setup"
-sudo yum install -y mysql-server
- sudo systemctl start mysqld
- sudo systemctl enable mysqld
+# sudo yum install -y mysql-server
+#  sudo systemctl start mysqld
+#  sudo systemctl enable mysqld
 
 # sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Chocoslam'";
 # sudo mysql -u root -pChocoslam -e "FLUSH PRIVILEGES";
 # echo "create db"
 # sudo systemctl restart mysqld
 # sudo mysql -u root -pChocoslam -e "CREATE DATABASE demoDb;"
+sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 echo "create users"
 sudo groupadd csye6225
 sudo useradd -M -s /usr/sbin/nologin -g csye6225 csye6225
@@ -18,10 +20,11 @@ sudo dnf module -y enable nodejs:20
 sudo dnf module -y install nodejs:20/common
 sudo dnf -y install unzip
 echo "change ownership"
-
 sudo chown -R "csye6225:csye6225"  "/opt/"
 ls -l /tmp
 sudo unzip "/tmp/webapp.zip" -d  "/opt/webapp"
+
+
 ls -l "/opt"
 cd /opt/webapp
 sudo npm install
