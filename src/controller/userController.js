d14a62eb72c7ee9e5ceb8ac9e14674f0323a5e3d
harshneu404 <agrawal.harsh@northeastern.edu>
2024-03-19 18:57:39 -0400

    update logs

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -9,10 +9,12 @@ const createUser = async(req,res)=>{
        
          if(Object.keys(req.query).length>0){
             res.status(400).send();
-   }
+            throw ({err:'Invalid Query',message:"The Query is Invalid"})
+        }
         const userExists = await User.findOne({where:{emailId:req?.body?.emailId}});
         if(userExists){
             res.status(400).send();
+            throw ({err:'User exists',message:`User with ${req.body.emailId} already exists`})
         }
 
         const user = {
@@ -24,9 +26,12 @@ const createUser = async(req,res)=>{
        
         const response = await User.create(user);
 
-       logger.log({
-        level: 'info',
-        message: ' User Created'
+       logger.info({
+        label:"User Data",
+        message: `User Created with id ${user.emailId}`,
+        httpRequest:{
+            httpMethod:'POST'
+        }
        })
         res.status(201).json({
             emailId:response.emailId,
@@ -36,6 +41,14 @@ const createUser = async(req,res)=>{
             updatedAt: response?.updatedAt
         });
     }catch(err){
+        logger.log({
+            label:"User Creation",
+            level: 'error',
+            message: err.message,
+            httpRequest:{
+                httpMethod:'POST'
+            }
+           })
         console.log(err);
         res.status(400).send();
     }
@@ -46,32 +59,39 @@ const updateUser = async(req,res)=>{
        
         if(Object.keys(req.query).length>0){
             res.status(400).send();
+            throw ({err:'Invalid Query',message:"The Query is Invalid"})
            }
         //handle empty body
         if(!req.headers){
             res.status(401).send();
+            throw ({err:'Unauthenticated',message:"user is not authenticated"})
         }
         const clientCredentials = verifyBasicAuth(req.headers);
        
         if(!clientCredentials){
             res.status(401).send();
+            throw ({err:'Unauthenticated',message:"user is not authenticated"})
         }
         const authenticatedUser =  await User.findOne({where:{emailId:clientCredentials.emailId}}); 
         if(!authenticatedUser){
             res.status(401).send();
+            throw ({err:'Unauthenticated',message:"user is not authenticated"})
         }
         
         const correctPassword =  bcrypt.compareSync(clientCredentials.password,authenticatedUser.password);
         if(!correctPassword){
             res.status(401).send();
+            throw ({err:'Unauthenticated',message:"user is not authenticated"})
         }
         if(!validateObject(req.body,["firstName","lastName","password"])){
             res.status(400).send();
+            throw ({err:'Invalid Query',message:"The Query is Invalid"})
         }
         const user = {};
 
         if(Object.keys(req.body).length == 0){
             res.status(400).send();
+            throw ({err:'Invalid Query',message:"The Query is Invalid"})
         }
        for(key in req.body){
         user[key] = req.body[key];
@@ -84,11 +104,22 @@ const updateUser = async(req,res)=>{
        console.log(response);
        logger.log({
         level: 'info',
-        message: ' User Updated'
+        message: `User ${clientCredentials.emailId} Updated`,
+        httpRequest:{
+            httpMethod:'PUT'
+        }
        })
        res.status(204).send();
     }
     catch(err){
+        logger.log({
+            label:"User Update",
+            level: 'error',
+            message: err?.message,
+            httpRequest:{
+                httpMethod:'PUT'
+            }
+           })
         console.log(err);
         res.status(400).send();
     }
@@ -103,24 +134,41 @@ const getUser = async(req,res)=>{
            }
         if(!req.headers){
             res.status(401).send();
+            throw ({err:'Unauthenticated',message:"user is not authenticated"})
         }
         const clientCredentials = verifyBasicAuth(req.headers);
        
         if(!clientCredentials){
             res.status(401).send();
+            throw ({err:'Unauthenticated',message:"user is not authenticated"})
+
         }
         const authenticatedUser =  await User.findOne({where:{emailId:clientCredentials.emailId}}); 
         if(!authenticatedUser){
             res.status(401).send();
+            throw ({err:'Unauthenticated',message:"user is not authenticated"})
+
         }
         
         const correctPassword =  bcrypt.compareSync(clientCredentials.password,authenticatedUser.password);
         if(!correctPassword){
             res.status(401).send();
+            throw ({err:'Unauthenticated',message:"user is not authenticated"})
+
         }
         if(req.is("*/*")){
-            return res.status(400).send();
+            res.status(400).send();
+            throw ({err:'Invalid Query',message:"The Query is Invalid"})
+
         }
+        logger.log({
+            label:"User Fetch",
+            level: 'info',
+            message: `User ${clientCredentials.emailId} fetched`,
+            httpRequest:{
+                httpMethod:'Get'
+            }
+           })
         res.status(200).json({
             emailId:authenticatedUser.emailId,
             firstName:authenticatedUser?.firstName,
@@ -130,6 +178,14 @@ const getUser = async(req,res)=>{
         });
 
     }catch(err){
+        logger.log({
+            label:"User Fetch",
+            level: 'error',
+            message: err?.message,
+            httpRequest:{
+                httpMethod:'Get'
+            }
+           })
         console.log(err);
         res.status(500).send();
     }
