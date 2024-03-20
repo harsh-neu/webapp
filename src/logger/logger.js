ef02efc31762698cec1bbbd8bf0c44057088037c
harshneu404 <agrawal.harsh@northeastern.edu>
2024-03-20 18:02:41 -0400

    update logger

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -16,15 +16,13 @@ const logger = winston.createLogger({
       ),
     // format:myFormat,
     // format: json({timestamp,label,level,message}),
-    transports: [
-        //
-        // - Write all logs with importance level of `error` or less to `error.log`
-        // - Write all logs with importance level of `info` or less to `combined.log`
-        //
-        new winston.transports.File({ filename: '/var/log/webapp/error.log', level: 'error' }),
-        new winston.transports.File({ filename: '/var/log/webapp/combined.log' }),
-        //  new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        // new winston.transports.File({ filename: 'combined.log' }),
-    ],
+    transports: process.env.ENVIRONMENT === "PRODUCTION" ?
+        [
+            new winston.transports.File({ filename: '/var/log/webapp/error.log', level: 'error' }),
+            new winston.transports.File({ filename: '/var/log/webapp/combined.log' })
+        ] :
+        [
+            new winston.transports.Console()
+        ]
 })
 module.exports= {logger};
